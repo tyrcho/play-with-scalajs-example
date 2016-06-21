b9ba4f5d1bae122028f2fe7d0b3c10564ea49571
Michel Daviot <michel.daviot@worldline.com>
2016-06-21 09:20:57 +0200

    offline application

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -1,5 +1,8 @@
 package controllers
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
 import play.api.Environment
 import play.api.mvc._
 import shared.SharedMessages
@@ -8,6 +11,22 @@ class Application()(implicit environment: Environment) extends Controller {
 
   def index = Action {
     Ok(views.html.index(SharedMessages.itWorks))
+  }
+
+  val cacheManifest = {
+    val date=new SimpleDateFormat("dd MMMM yyyy hh:mm:ss").format(new Date)
+    Action { implicit request =>
+      Ok(
+        s"""CACHE MANIFEST
+            |# $date
+            |${routes.Assets.at("stylesheets/main.css").url}
+            |${routes.Assets.at("lib/jquery/jquery.min.js").url}
+            |/assets/client-jsdeps.js
+            |/assets/client-fastopt.js
+            |/assets/client-opt.js
+            |/assets/client-launcher.js
+         """.stripMargin).as("text/cache-manifest")
+    }
   }
 
 }
